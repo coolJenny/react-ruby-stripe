0998048163c0edcb53590facb202bb0b9fad7cd7
coolJenny <coolJenny009g@gmail.com>
2018-05-15 22:58:44 -0400

    ..

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-	:publishable_key => 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
-	:secret_key		 => 'sk_test_BQokikJOvBiI2HlWgH4olfQ2'
+	:publishable_key => Rails.application.secrets.stripe_publishable_key,
+	:secret_key		 => Rails.application.secrets.stripe_secret_key
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.application.secrets.stripe_secret_key
